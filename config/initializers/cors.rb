b6b6ff15ae429565fb6d04b5b63ad687c1531c39
Cristopher Huaroc <cristopheer96@gmail.com>
2025-03-14 23:46:52 -0500

    adding cors and config deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://tu-aplicacion-react.com'  # Cambia por el dominio de tu frontend. Para desarrollo, puede ser http://localhost:3000
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['access-token', 'client', 'uid']  # Si usas Devise Token Auth, por ejemplo.
+  end
+end
